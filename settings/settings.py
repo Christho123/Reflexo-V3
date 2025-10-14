4786e0b342c62237df22f9b0bcd4c0d033e2376c
Christhoper <qrqdsq45@gmail.com>
2025-10-14 09:01:07 -0500

    Update settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Reflexo-V3',
         'USER': 'root',
-        'PASSWORD': 'Reflexo159//',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
